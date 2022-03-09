0ac64cf380fe9b111df1f0ff2551ae724a86778f
Spencer Brazell <sa.brazell11@gmail.com>
2022-03-08 19:18:47 -0700

    Arrays are Working

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,9 +18,14 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(function(element) {
+    return element % 2 === 0
+})     
+console.log(evenNumbers)
 
-
+// = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(num => num % 2 === 0)
+console.log(evenNumbers)
 
 ////////// PROBLEM 2 //////////
 
@@ -41,6 +46,13 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 // CODE HERE
 const postTaxPrices // = prices.map(/* Provide Your Callback Here );
 
+const postTaxPrices = prices.map(function(element){
+      return element * 1.07
+  })
+  console.log(postTaxPrices)
+
+  const postTaxPrices = prices.map(num => num * 1.07)  
+    console.log(postTaxPrices)
 
 
 ////////// PROBLEM 3 //////////
@@ -57,8 +69,15 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce(function(acc, element){
+    return acc + element
+})
+console.log(totalPopulation)
 
+const totalPopulation = populations.reduce((a, c) => a + c)
+
+console.log(totalPopulation)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,8 +101,14 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+// const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(function(element){
+    return element.CP > 200
+})
+console.log(myStrongest)
 
+const myStrongest = monstersInYourPocket.filter(monster => monster.CP > 200)
+console.log(myStrongest)
 
 
 ////////// PROBLEM 5 //////////
@@ -100,7 +125,13 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
+const orderTotals = orders.map(function(element){
+    return element.price + (element.price * element.tax)
+})
+console.log(orderTotals)
 
+const orderTotals = orders.map(element => element.price + (element.price * element.tax))
+console.log(orderTotals)
 
 
 ////////// PROBLEM 6 //////////
@@ -120,3 +151,14 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+
+const bobsTotal = purchases.filter(function(element){
+  return element.owner === "Bob"
+}) .reduce(function(acc, element){
+  return acc + element.price
+}, 0)
+console.log(bobsTotal)
+
+const bobsTotal = purchases
+    .filter(purchase => purchase.owner === "Bob")
+    .reduce((a, c) => a + c.price, 0)
